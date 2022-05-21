357ec40e88084af9a932af9e57f48df68953c679
Indeed <gravitybartek@gmail.com>
2022-05-21 10:04:37 +0200

    Added score counter incrementing every second

--- scripts.js
+++ scripts.js
@@ -23,6 +23,8 @@ let isAlive = setInterval(function()
     //dino y and cactus x
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+    
+    //Collision detection
     if(cactusLeft < dinoWidth && cactusLeft > 0 && dinoTop >= dinoClouds)
     {
         cactus.style.animation = 'none';
@@ -33,7 +35,23 @@ let isAlive = setInterval(function()
     }
 }, 10);
 
-//detect collision
-
 document.addEventListener("keydown", 
     function(event){ jump()})
+
+//Timer
+const timer = document.getElementById('timer');
+timer.innerHTML = "1";
+
+let timerInterval = window.setInterval(function(){
+    let newValue = parseInt(timer.innerHTML) + 1;
+    timer.innerHTML = String(padLeadingZeros( newValue, 4));
+}, 1000);
+
+//adds zeros to number
+function padLeadingZeros(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+padLeadingZeros(57, 3);// "057"
+padLeadingZeros(57, 4); //"0057"
